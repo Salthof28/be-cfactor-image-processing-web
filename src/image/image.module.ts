e4597f5091540f1069b514a0da141748509f2dc3
Salman <salmanalthof31@gmail.com>
2026-06-30 19:00:37 +0700

    feat: register queue in ImageModule

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
+  imports: [BullModule.registerQueue({
+    name: 'imageProcessQueue'
+  })],
   controllers: [ImageController],
   providers: [ImageService],
 })
