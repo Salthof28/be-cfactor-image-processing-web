5aedfaeee35471a9e6a5f6d6a0f62fa017b4f582
Salman <salmanalthof31@gmail.com>
2026-07-01 21:07:48 +0700

    refactor: improve error messages and add developer logging

--- src/image/image.processor.ts
+++ src/image/image.processor.ts
@@ -1,5 +1,5 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { Job } from 'bullmq'
 import { ImageProcessingServiceItf } from './services/image-processing.service.interface';
 import { ImageStorageServiceItf } from './services/image-storage.service.interface';
@@ -7,17 +7,19 @@ import { ImageStorageServiceItf } from './services/image-storage.service.interfa
 @Injectable()
 @Processor('imageProcessQueue')
 export class ImageProcessor extends WorkerHost {
+    private readonly logger = new Logger(ImageProcessor.name);
     constructor(@Inject('ImageProcessingServiceItf') private readonly imageProcessingService: ImageProcessingServiceItf, @Inject('ImageStorageServiceItf') private readonly imageStorageService: ImageStorageServiceItf){ super() }
     async process (job: Job, token?: string): Promise<void> {
         switch(job.name) {
             case "compress":
-                this.imageProcessingService.compress(job.data);
+                await this.imageProcessingService.compress(job.data);
                 break;
             case "delete":
-                this.imageStorageService.delete(job.data);
+                await this.imageStorageService.delete(job.data);
                 break;
             default:
-                throw new InternalServerErrorException(`unknown job: ${job.name}`)
+                this.logger.error(`Unsupported job type "${job.name}" for job "${job.id}".`);
+                throw new InternalServerErrorException('Image processing failed due to an internal error.')
         }
     }
 }
