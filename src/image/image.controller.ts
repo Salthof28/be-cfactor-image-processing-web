6ddcf95f499f39dddf89f1af9cc4ea5f300513df
Salman <salmanalthof31@gmail.com>
2026-07-01 09:50:17 +0700

    feat: call upload from image.service to validation file and save in storage

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ImageService } from './image.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CustomExceptionGen } from 'src/common/exception/exception.general';
+import { StatusImageDto } from './dto/res/status-image.dto';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload')
+  async uploadImg(@UploadedFile() file: Express.Multer.File): Promise<StatusImageDto> {
+    try {
+      return await this.imageService.upload(file)
+    } catch (error) {
+      if(error instanceof CustomExceptionGen) throw error;
+      throw new InternalServerErrorException()
+    }
+  }
 }
