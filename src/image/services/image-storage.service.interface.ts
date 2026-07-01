efce7a2d130e6c5ec8cb4fd9d06636c7abd7075e
Salman <salmanalthof31@gmail.com>
2026-07-01 19:05:48 +0700

    feat: create logic delete file as not use

--- src/image/services/image-storage.service.interface.ts
+++ src/image/services/image-storage.service.interface.ts
@@ -1,4 +1,10 @@
 
 export interface ImageStorageServiceItf {
     upload(img: Express.Multer.File, jobId: string): Promise<string>
+    delete(data: DataDelete): Promise<void>
+}
+export interface DataDelete {
+    jobId: string,
+    uploadPath: string,
+    processedPath: string
 }
--- src/image/services/image-storage.service.ts
+++ src/image/services/image-storage.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from "@nestjs/common";
-import { ImageStorageServiceItf } from "./image-storage.service.interface";
+import { DataDelete, ImageStorageServiceItf } from "./image-storage.service.interface";
 import { extname, join } from "path";
 import { STORAGE_DIR, UPLOAD_DIR } from "../constants/image.constants";
-import { writeFile } from "fs/promises";
+import { unlink, writeFile } from "fs/promises";
 
 
 @Injectable()
 export class ImageStorageService implements ImageStorageServiceItf  {
-    constructor(){}
-    
     async upload(img: Express.Multer.File, jobId: string): Promise<string> {
         const uploadPath = join(process.cwd(), STORAGE_DIR, UPLOAD_DIR, `${jobId}${extname(img.originalname)}`);
         await writeFile(uploadPath, img.buffer);
         return uploadPath;
     }
-    
+
+    async delete(data: DataDelete): Promise<void> {
+        await unlink(data.uploadPath);
+        await unlink(data.processedPath);
+    }
 }
+
+// for download
+// return new StreamableFile(createReadStream(filePath));
