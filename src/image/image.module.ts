9b22e1b938957a232dd38d540a595bb4ba41a77d
Salman <salmanalthof31@gmail.com>
2026-07-01 19:11:28 +0700

    feat: register image-processing.service and image-storage.service

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
 import { BullModule } from '@nestjs/bullmq';
+import { ImageProcessor } from './image.processor';
+import { ImageProcessingService } from './services/image-processing.service';
+import { ImageStorageService } from './services/image-storage.service';
 
 @Module({
   imports: [BullModule.registerQueue({
     name: 'imageProcessQueue'
   })],
   controllers: [ImageController],
-  providers: [ImageService],
+  providers: [
+    { provide: 'ImageServiceItf', useClass: ImageService },
+    { provide: 'ImageProcessingServiceItf', useClass: ImageProcessingService },
+    { provide: 'ImageStorageServiceItf', useClass: ImageStorageService },
+    ImageProcessor
+  ],
 })
 export class ImageModule {}
