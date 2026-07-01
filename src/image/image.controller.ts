9815790cd7fb61e88107dc257b3f25d282030911
Salman <salmanalthof31@gmail.com>
2026-07-01 19:13:30 +0700

    refactor: change handle unknown error in catch

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { ImageService } from './image.service';
+import { Controller, Inject, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CustomExceptionGen } from 'src/common/exception/exception.general';
 import { StatusImageDto } from './dto/res/status-image.dto';
+import { ImageServiceItf } from './image.service.interface';
 
 @Controller('image')
 export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
+  constructor(@Inject('ImageServiceItf') private readonly imageService: ImageServiceItf) {}
 
   @UseInterceptors(FileInterceptor('file'))
   @Post('upload')
@@ -15,7 +15,7 @@ export class ImageController {
       return await this.imageService.upload(file)
     } catch (error) {
       if(error instanceof CustomExceptionGen) throw error;
-      throw new InternalServerErrorException()
+      throw new InternalServerErrorException(error.message)
     }
   }
 }
