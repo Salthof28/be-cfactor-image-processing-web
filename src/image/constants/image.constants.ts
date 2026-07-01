67e0113c920fc75ac74e69a1525b425bedf111b1
Salman <salmanalthof31@gmail.com>
2026-07-01 21:09:35 +0700

    feat: add IMAGE_PROCESS_MESSAGE for massage job status

--- src/image/constants/image.constants.ts
+++ src/image/constants/image.constants.ts
@@ -4,6 +4,12 @@ export const ALLOWED_IMAGE_MIMETYPES = [
     'image/webp',
 ];
 
+export const IMAGE_PROCESS_MESSAGES = {
+    waiting: 'Image upload received. Waiting to be processed.',
+    active: 'Image is being processed.',
+    completed: 'Image processed successfully.',
+}
+
 export const MAX_SIZE_IMAGE = 20 * 1024 * 1024;
 export const STORAGE_DIR = 'storage';
 export const UPLOAD_DIR = 'uploads';
